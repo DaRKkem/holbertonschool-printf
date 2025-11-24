c595726ee717f43568fe996f166a88c01c003e3b
DaRKkem <damross446@gmail.com>
2025-11-24 11:19:31 +0100

    commit

--- _printf.c
+++ _printf.c
@@ -4,13 +4,21 @@
 #include <stdarg.h>
 #include "main.h"
 
+
+
 /**
  * _printf - prints a string and arguments (if given)
  *
  * Return: Always nothing.
  */
-int _printf(const char *format, ...)
+int _printf(const char *format, ..)
 {
-	
+	int i = 0;
+	va_list args; /* args = liste des arguments*/
+
+	va_start(args, format);
+
+	va_arg(args, char);
+
 	return (0);
 }

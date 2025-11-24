865acfecb942ebfff23f2ec9f91e9fb28a276cca
DaRKkem <damross446@gmail.com>
2025-11-24 09:38:20 +0100

    commit

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include "prototypes.h"
+
+/**
+ * _printf - prints a string and arguments (if given)
+ *
+ * Return: Always nothing.
+ */
+void _printf()
+{
+	return;
+}
---
+++ prototypes.h
@@ -0,0 +1,6 @@
+#ifndef PROTOTYPES
+#define PROTOTYPES
+
+void _printf();
+
+#endif /* PROTOTYPES_H */

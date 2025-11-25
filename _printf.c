4661c15ce848f865364191accb8a977dfe7b1932
DaRKkem <damross446@gmail.com>
2025-11-25 15:22:14 +0100

    commit

--- _printf.c
+++ _printf.c
@@ -1,57 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-#include "main.h"
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <stdarg.h>
+	#include "main.h"
 
-/**
- * _printf - prints a string and arguments (if given)
- *
- * Return: Always nothing.
- */
-int _printf(const char *format, ...)
-{
-	int i = 0, compte = 0;
-	va_list args;
-	int (*func)(va_list);
-
-	if (!format[i])
-		return (-1);
-	
-	if (format[i] == '%' && !format[i + 1])
-		compte++;
-
-	va_start(args, format);
-
-	while (format[i])
+	/**
+	 * _printf - prints a string and arguments (if given)
+	 *
+	 * Return: Always nothing.
+	 */
+	int _printf(const char *format, ...)
 	{
-		if (format[i] == '%' && format[i + 1])
-		{
-			i++;
-			func = get_func(format[i]);
+		int i = 0, compte = 0;
+		va_list args;
+		int (*func)(va_list);
 
-			if (func)
+		if (!format[i])
+			return (-1);
+		
+		if (format[i] == '%' && !format[i + 1])
+		{
+			return (1);
+		}
+
+		va_start(args, format);
+
+		while (format[i])
+		{
+			if (format[i] == '%' && format[i + 1])
 			{
-				compte += func(args);
-			}
-			else if (format[i] == '%')
-			{
-				compte += _putchar('%');
+				i++;
+				func = get_func(format[i]);
+
+				if (func)
+				{
+					compte += func(args);
+				}
+				else if (format[i] == '%')
+				{
+					compte += _putchar('%');
+				}
+				else
+				{
+					compte += _putchar('%');
+					compte += _putchar(format[i]);
+				}
 			}
 			else
 			{
-				compte += _putchar('%');
 				compte += _putchar(format[i]);
 			}
+			i++;
 		}
-		else
-		{
-			compte += _putchar(format[i]);
-		}
-		i++;
+
+		va_end(args);
+
+		return (compte);
 	}
-
-	va_end(args);
-
-	return (compte);
-}
